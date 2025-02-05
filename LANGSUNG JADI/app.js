9bf610c8ac877194fa14bc4a05312e7785d83dd7
Auliya Apriwiguna <apri.wiguna29@gmail.com>
2025-02-06 06:05:50 +0700

    add new projek boottstrap

---
+++ LANGSUNG JADI/app.js
@@ -0,0 +1,28 @@
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('navbarSupportedContent')
+// const bsCollapse = new bootstrap.Collapse(menuToggle)
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { bsCollapse.toggle() })
+// })
+
+// const sections = document.querySelectorAll('section');
+// const navli = document.querySelectorAll('nav .container-okee ul li');
+
+// window.addEventListener('scroll', () => {
+//     let current = '';
+//     sections.forEach(section => {
+//         const sectionTop = section.offsetTop;
+//         const sectionHeight = section.clientHeight;
+//         if (pageYOffset >= sectionTop) {
+//             current = section.getAttribute('id');
+//         }
+//     })
+
+//     navli.forEach(li => {
+//         li.classList.remove('active');
+//         if (li.classList.contains(current)) {
+//             li.classList.add('active')
+//         }
+//     })
+// })
+// file:///C:/Users/asus/Documents/Untuk%20Video.html 16:58
